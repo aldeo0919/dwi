492c67f2ef57b16ec205998a1f41cce3f12d3942
aldeo0919 <84286483+aldeo0919@users.noreply.github.com>
2021-05-17 15:04:23 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,5 @@
 # veruscoin
-apt update
+sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u BecJeQaRQvW9FmqAgyQRSNwk3TDD9MUCrK.tes -t1
